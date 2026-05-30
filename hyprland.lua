0d5b154b0137bd3b62273c3d5c08cd5f47c5460b
Alex Joedt <84435160+alexjoedt@users.noreply.github.com>
2026-05-30 11:31:18 +0200

    feat: update autostart configuration to include gnome-keyring-daemon

--- hyprland.lua
+++ hyprland.lua
@@ -44,11 +44,12 @@ local menu        = "wofi --show drun"
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 
--- hl.on("hyprland.start", function()
---     hl.exec_cmd(terminal)
---     hl.exec_cmd("nm-applet")
---     hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+hl.on("hyprland.start", function()
+    hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
+    --hl.exec_cmd(terminal)
+    --hl.exec_cmd("nm-applet")
+    --hl.exec_cmd("waybar & hyprpaper & firefox")
+end)
 
 
 -----------------------------

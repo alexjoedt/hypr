2336830fe147e2c5546b7c258566264f8e2af5b5
Alex Joedt <84435160+alexjoedt@users.noreply.github.com>
2026-05-30 08:21:27 +0200

    feat: add initial configuration for Dell Gaming Wide monitor

---
+++ hyprland.lua
@@ -0,0 +1,9 @@
+-- Dell Gaming Wide
+-- 3440x1440
+-- DisplayPort: 144Hz
+-- HDMI: 100Hz
+-- position = "0x0", -- poition in pixels 0x0 -> upper left corner scale = 1,
+-- hl.monitor({output = "DP-1", mode = "3440x1440@144", position = "auto", scale = 1})
+
+-- Random monitor
+hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1 })

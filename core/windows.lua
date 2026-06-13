fb56f14426c3352b87da2c02a3f3413d43e6f7fd
Alex Joedt <84435160+alexjoedt@users.noreply.github.com>
2026-06-13 06:52:58 +0200

    feat: update workspace rule to ensure no gaps on workspace 1

--- core/windows.lua
+++ core/windows.lua
@@ -6,7 +6,7 @@ function M.setup()
 
     -- Example window rule
     -- hl.window_rule({ match = { class = "kitty", title = "kitty" }, float = true })
-
+    hl.workspace_rule({ workspace = "1", layout = "scrolling" }) -- no gaps on workspace 1
     -- Ignore maximize requests from apps. You'll probably like this.
     -- hl.window_rule({
     --     name  = "suppress-maximize-events",

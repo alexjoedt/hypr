8bd6902d21d0e0e97bbf8c4d2625658294cb1097
Alex Joedt <84435160+alexjoedt@users.noreply.github.com>
2026-06-27 10:55:59 +0200

    use eu

--- core/input.lua
+++ core/input.lua
@@ -1,34 +1,34 @@
 local M = {}
 
 function M.setup()
-    -- https://wiki.hypr.land/Configuring/Basics/Variables/#input
-    hl.config({
-        input = {
-            kb_layout  = "de",
-            kb_variant = "",
-            kb_model   = "",
-            kb_options = "",
-            kb_rules   = "",
+	-- https://wiki.hypr.land/Configuring/Basics/Variables/#input
+	hl.config({
+		input = {
+			kb_layout = "eu",
+			kb_variant = "",
+			kb_model = "",
+			kb_options = "",
+			kb_rules = "",
 
-            follow_mouse = 1,
+			follow_mouse = 1,
 
-            sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+			sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
 
-            touchpad = {
-                natural_scroll = false,
-            },
-        },
-    })
+			touchpad = {
+				natural_scroll = false,
+			},
+		},
+	})
 
-    -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Gestures
-    hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
+	-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Gestures
+	hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
 
-    -- Example per-device config
-    -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
-    hl.device({
-        name        = "epic-mouse-v1",
-        sensitivity = -0.5,
-    })
+	-- Example per-device config
+	-- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
+	hl.device({
+		name = "epic-mouse-v1",
+		sensitivity = -0.5,
+	})
 end
 
 return M
